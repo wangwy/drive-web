b9ab7fd0d90540cb2e355449371b10535bd8a225
wangwy <wangwenyuan@goodow.com>
2014-05-09 13:32:32 +0800

    文档行为管理添加搜索功能,添加gulp压缩功能,完成travis单元测试未完成
    Change-Id: I88296c25672f8a69920a0cfd7d71913264523a77

--- app/js/services.js
+++ app/js/services.js
@@ -3,46 +3,10 @@
 /* Services */
 
 
-
 angular.module('drive.services', [])
-  .factory('DataGridService', function(){
-        var service = {
-            getData: function ( index,type,pageNo,size ,callback) {
-                var options = {debug:true,forkLocal:true};
-                var goodUrl = 'http://test.goodow.com:8080/eventbus';
-                var goodChannel = 'realtime.search';
-                var bus = new good.channel.WebSocketBus(goodUrl,options);
-
-                bus.registerHandler("@good.bus.onOpen", function(message) {
-                    console.log("Opened at: " + new Date().toString());
-                });
-
-                bus.registerHandler("@good.bus.onClose", function(message) {
-                    console.log("Closed at: " + new Date().toString());
-                });
-
-                bus.registerHandler("@good.bus.onError", function(error) {
-                    console.log("Error at: " + error);
-                });
-                var queryOptions1 = {
-                    action:'search',
-                    _index:index,
-                    _type:type,
-                    source:{
-                        query:{
-                            match_all:{
-
-                            }
-                        },
-                        from:(pageNo-1)*size,
-                        size:size
-                    },
-                    search_type:'query_then_fetch',
-                    scroll:'5m'
-                };
-                console.log(queryOptions1);
-                bus.send(goodChannel,queryOptions1,callback);
-            }
-        }
-        return service;
-  });
+    .factory('DataGridService', function () {
+        var options = {debug: true, forkLocal: true};
+        var goodUrl = 'http://realtime.goodow.com:8080/eventbus';
+        var bus = new good.channel.WebSocketBus(goodUrl, options);
+        return bus;
+    });
